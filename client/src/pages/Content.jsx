7f6e788e4de10d939a2511a11343fe1bb669cfd4
Strangebrewer <BKAShambala@gmail.com>
2019-12-01 01:17:31 -0700

    fiddling with readme file

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -13,7 +13,7 @@ const Content = props => {
          <h3>You should only be able to see it if you are logged in.</h3>
          <h2>See for yourself : &nbsp;<Button onClick={props.logout}><code>Logout</code></Button></h2>
          <h3>You can also go back to the <Link to="/"><code>landing</code></Link> page</h3>
-         <h3>But, you cannot go to the <Link to="/"><code>login</code></Link> page; it will just redirect you to the landing page</h3>
+         <h3>But, since you're logged in, you cannot go to the <Link to="/"><code>login</code></Link> page - it will just redirect you to the landing page</h3>
          <h3>You can pass props through the second prop of the Authentication HOC</h3>
          <h4>Like so:</h4>
          <p>{props.otherProps}</p>
